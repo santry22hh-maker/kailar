feadeac9b6b93436a90b500bbc965a3a97edc45e
Hardi22 <santry22.hh@gmail.com>
2025-11-14 00:08:13 +0900

    Initial commit

---
+++ app/View/Components/Sidebar.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Sidebar extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public $links;
+
+    public function __construct()
+    {
+        $this->links = [
+            [
+            'label' => 'Dashboard', 
+            'icon' => 'fas fa-home',
+            'rute' => 'Dashboard',
+            'is_dropdown' => false,
+            'is_active' => request()->is('dashboard*'),
+         ],
+        ];
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.sidebar');
+    }
+}
---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,31 @@
+<div class="sidebar-content">
+    <ul class="nav nav-secondary">
+        @foreach ($links as $link)
+            @if ($link['is_dropdown'])
+                <li class="nav-item {{ $link['is_active'] ? active : '' }}">
+                    <a data-bs-toggle="collapse" href="#dashboard" class="collapsed" aria-expanded="false">
+                        <i class="{{ $link['icon'] }}"></i>
+                        <p>{{ $link['label'] }}</p>
+                        <span class="caret"></span>
+                    </a>
+                    <div class="collapse" id="dashboard">
+                        <ul class="nav nav-collapse">
+                            <li>
+                                <a href="../demo1/index.html">
+                                    <span class="sub-item">Dashboard 1</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+            @else
+                <li class="nav-item {{ $link['is_active'] ? active : '' }}">
+                    <a href="widgets.html">
+                        <i class="{{ $link['icon'] }}"></i>
+                        <p>{{ $link['label'] }}</p>
+                    </a>
+                </li>
+            @endif
+        @endforeach
+    </ul>
+</div>
